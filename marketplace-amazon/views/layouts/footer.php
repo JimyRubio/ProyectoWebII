169fccdc940d7c194c3f0955e1565933c3bb76c6
Jonx16 <jonathanhn709@gmail.com>
2026-07-22 21:11:01 -0600

    Update footer.php
    Código para cargar las plantillas HTML, hojas de estilo (CSS) y librerías clave (jQuery, FontAwesome, ect.)

---
+++ marketplace-amazon/views/layouts/footer.php
@@ -0,0 +1,14 @@
+</main> <!-- Cierre de container -->
+
+    <footer class="main-footer">
+        <p>&copy; <?php echo date('Y'); ?> <?php echo APP_NAME; ?> - Todos los derechos reservados.</p>
+    </footer>
+
+    <!-- jQuery CDN (Obligatorio por los requerimientos del proyecto) -->
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    
+    <!-- JS Globales y Utilidades AJAX -->
+    <script src="<?php echo BASE_URL; ?>public/js/utils.js"></script>
+    <script src="<?php echo BASE_URL; ?>public/js/main.js"></script>
+</body>
+</html>
