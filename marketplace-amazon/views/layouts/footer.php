e9d929de53cebb9e5bf98b50851f31301c438a2e
Jonx16 <jonathanhn709@gmail.com>
2026-07-23 18:40:46 -0600

    Update footer.php
    Se agregó la inclusión de Chart.js CDN y la carga dinámica de scripts de módulo cuando se defina $module_js.

--- marketplace-amazon/views/layouts/footer.php
+++ marketplace-amazon/views/layouts/footer.php
@@ -7,8 +7,16 @@
     <!-- jQuery CDN (Obligatorio por los requerimientos del proyecto) -->
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     
+    <!-- Chart.js CDN (Para dashboards y gráficas) -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    
     <!-- JS Globales y Utilidades AJAX -->
     <script src="<?php echo BASE_URL; ?>public/js/utils.js"></script>
     <script src="<?php echo BASE_URL; ?>public/js/main.js"></script>
+
+    <?php if (isset($module_js) && !empty($module_js)): ?>
+        <!-- JS Módulo Específico -->
+        <script src="<?php echo BASE_URL; ?>public/js/modules/<?php echo $module_js; ?>"></script>
+    <?php endif; ?>
 </body>
 </html>
