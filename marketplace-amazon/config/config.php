7734bc2dda249529517e594ad3ed416948231436
JimyRubio <jimya.rubio@gmail.com>
2026-07-29 19:53:04 -0600

    Cambio base de datos
    se Cambio la contra a la base de datos

--- marketplace-amazon/config/config.php
+++ marketplace-amazon/config/config.php
@@ -45,7 +45,7 @@ if (session_status() === PHP_SESSION_NONE) {
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'marketplace_db');
 define('DB_USER', 'root');
-define('DB_PASS', 'password');
+define('DB_PASS', '');
 define('DB_CHARSET', 'utf8mb4');
 
 // URL Base Exacta
